16de383e630793e25904c21ab73b87d4d425378b
Jeroen Ooms <jeroenooms@gmail.com>
2022-08-25 10:43:13 +0200

    Use devel branch of bioc registry

--- R/cran.R
+++ R/cran.R
@@ -20,7 +20,8 @@ cran_registry <- function(){
 bioc_registry <- function(){
   # Bioc does not seem to have a packages.rds containing URL and BugReports
   # Bioc registry version can be overridden with R_BIOC_VERSION
-  bioc_version <- as.character(tools:::.BioC_version_associated_with_R_version())
+  # bioc_version <- as.character(tools:::.BioC_version_associated_with_R_version())
+  bioc_version <- "3.16" # Use devel branch of registry, to get latest package metadata
   bioc <- jsonlite::read_json(sprintf('https://bioconductor.org/packages/json/%s/bioc/packages.json', bioc_version))
   names(bioc) <- NULL
   packages <- jsonlite:::simplify(bioc)
